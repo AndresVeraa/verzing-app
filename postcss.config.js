eacc12603967602f5ce81d9e124e75cf32fdc411
AndresVeraa <osmarandresvera@gmail.com>
2026-01-05 23:15:54 -0500

    2 comit

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,5 @@
 export default {
-  // Configuración de los plugins de procesamiento de CSS
   plugins: {
-    // Activa el motor de Tailwind CSS para procesar las clases
-    tailwindcss: {},
-    // Añade prefijos de compatibilidad para diferentes navegadores automáticamente
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
   },
-};
+}
